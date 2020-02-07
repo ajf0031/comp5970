cd83f507646a4ab7c93968a4e40f0b0ddef62bd4
Andrew Ford <ajf0031@auburn.edu>
2020-02-07 13:45:11 -0600

    Formatted, added more desriptive variable names

--- project1/project1.py
+++ project1/project1.py
@@ -1,18 +1,15 @@
 import sys
-
  
 BLOSUM_MATRIX = []
-INDEX_KEY = {}
-
-CHUNK_SIZE = 80 #max number of characters printed per line
+BLOSUM_MAP = {} #maps the blosum amino acid characters to integers
+MAX_CHARS_PER_LINE = 80 #max number of characters printed per line
 
 def main():
 	read_blosum()
-	read_sequences()
 	seq1, seq2 = read_sequences()
-
 	needleman_wunsch(seq1, seq2)
 	smith_waterman(seq1, seq2)
+
 
 def read_blosum():
 	try:
@@ -23,10 +20,12 @@ def read_blosum():
 	blosum_file.readline()
 	for line in blosum_file: 
 		char = line.split()
-		INDEX_KEY[char[0]] = index
+		BLOSUM_MAP[char[0]] = index
 		index += 1
 		BLOSUM_MATRIX.append([int(x) for x in char[1:]])
+	
 	blosum_file.close()
+
 
 def read_sequences():
 	if len(sys.argv) == 2:
@@ -47,11 +46,13 @@ def read_sequences():
 	else:
 		file1 = "pair1//1k4rA_dengue_virus.fasta"
 		file2 = "pair1//5ire_zika_virus.fasta"
+
 	try:	
 		fp1 = open(file1, "r")
 		fp2 = open(file2, "r")
 	except:
 		exit("Error opening file. Please make sure to include the proper path of the file")
+	
 	sequence1_list = fp1.readlines()
 	sequence2_list = fp2.readlines()
 	sequence1 = "" 
@@ -63,41 +64,42 @@ def read_sequences():
 		sequence2 += sequence2_list[line].strip()
 
 	fp1.close()
-	fp2.close()
-			
+	fp2.close()	
+		
 	return sequence1, sequence2
 
-def needleman_wunsch(seq1, seq2):
+
+def needleman_wunsch(seq1, seq2):	
+	value_map = { 0: "diagonal", 1: "left", 2: "up"}
 	
 	#BASE CASE for value_matrix and direction_matrix
 	value_matrix = [[0 for x in range(len(seq1)+1)] for y in range(len(seq2)+1)]
 	direction_matrix = [["" for x in range(len(seq1)+1)] for y in range(len(seq2)+1)]
 	
 	for i in range(1, len(seq1) + 1):	
-		blosum_index = INDEX_KEY[seq1[i-1]] #extracts the blosum index from the sequence	
+		blosum_index = BLOSUM_MAP[seq1[i-1]] #extracts the blosum index from the sequence	
 		value_matrix[0][i] = value_matrix[0][i-1] + BLOSUM_MATRIX[-1][blosum_index]
 		direction_matrix[0][i] = "left"	
 	for j in range(1, len(seq2) + 1):
-		blosum_index = INDEX_KEY[seq2[j-1]]
+		blosum_index = BLOSUM_MAP[seq2[j-1]]
 		value_matrix[j][0] = value_matrix[j-1][0] + BLOSUM_MATRIX[blosum_index][-1]
 		direction_matrix[j][0] = "up"
 	
 	#general case
 	for i in range(1, len(seq1) + 1):
-		blosum_index_i = INDEX_KEY[seq1[i-1]]
+		blosum_index_i = BLOSUM_MAP[seq1[i-1]]
 		for j in range(1, len(seq2) + 1):
-			blosum_index_j = INDEX_KEY[seq2[j-1]]
+			blosum_index_j = BLOSUM_MAP[seq2[j-1]]
+			
 			#find the 3 potential values that V(i,j) can be
 			diagonal = BLOSUM_MATRIX[blosum_index_j][blosum_index_i] + value_matrix[j-1][i-1]
 			vertical = BLOSUM_MATRIX[blosum_index_j][-1] + value_matrix[j-1][i]
 			horizontal = BLOSUM_MATRIX[-1][blosum_index_i] + value_matrix[j][i-1]
 
 			value_choice = max([diagonal, horizontal, vertical])
-			value_map = { 0: "diagonal", 1: "left", 2: "up"}
-		
 			value_matrix[j][i] = value_choice
 			
-			#put in the direction of the max score
+			#put in the direction of the max score- Note for duplicates, diagonal is chosen first
 			direction_matrix[j][i] = value_map[[diagonal, horizontal, vertical].index(value_choice)]
 
 	s1, match, s2 = backtrack(seq1, seq2, len(seq1), len(seq2), value_matrix, direction_matrix)
@@ -108,7 +110,6 @@ def needleman_wunsch(seq1, seq2):
 
 
 def smith_waterman(seq1, seq2):
-
 	max_value = 0
 	max_value_i = 0
 	max_value_j = 0
@@ -122,10 +123,10 @@ def smith_waterman(seq1, seq2):
 	
 	#general case
 	for i in range(1, len(seq1) + 1):
-		blosum_index_i = INDEX_KEY[seq1[i-1]]
+		blosum_index_i = BLOSUM_MAP[seq1[i-1]]
 		for j in range(1, len(seq2) + 1):
-			blosum_index_j = INDEX_KEY[seq2[j-1]]
-			#find the 3 potential values that V(i,j) can be, with the minimum they can be set to 0
+			blosum_index_j = BLOSUM_MAP[seq2[j-1]]
+			#find the 3 potential values that V(i,j) can be. The minimum they can be set to is 0.
 			diagonal = max(BLOSUM_MATRIX[blosum_index_j][blosum_index_i] + value_matrix[j-1][i-1], 0)
 			vertical = max(BLOSUM_MATRIX[blosum_index_j][-1] + value_matrix[j-1][i], 0)
 			horizontal = max(BLOSUM_MATRIX[-1][blosum_index_i] + value_matrix[j][i-1], 0)
@@ -137,16 +138,17 @@ def smith_waterman(seq1, seq2):
 				max_value = value_choice		
 			value_matrix[j][i] = value_choice
 			
-			#put in the direction of the max score, but only if if is greater than 0, otherwise there is no direction
+			#put in the direction of the max score, but only if if is greater than 0, 
+			# otherwise there is no direction
 			if value_choice > 0:
 				direction_matrix[j][i] = value_map[[diagonal, horizontal, vertical].index(value_choice)]
 
-	
 	s1, match, s2 = backtrack(seq1, seq2, max_value_i, max_value_j, value_matrix, direction_matrix)
 	
 	print("Smith Waterman Local Alignment")
 	print("Score: %s" % max_value)
 	print_alignment(s1, match, s2)
+
 
 def backtrack(seq1, seq2, start_x, start_y, value_matrix, direction_matrix):
 	#s1 and s2 are the char arrays with gaps
@@ -182,25 +184,21 @@ def backtrack(seq1, seq2, start_x, start_y, value_matrix, direction_matrix):
 
 	return s1, match, s2
 
-def print_alignment(sequence1, match_characters, sequence2):
 
+def print_alignment(sequence1, match_characters, sequence2):
 	index = 0
 	remaining_list_size = len(sequence1)
-	while CHUNK_SIZE < remaining_list_size:
-		print (''.join(sequence1[index:index+CHUNK_SIZE]))
-		print (''.join(match_characters[index:index+CHUNK_SIZE]))
-		print (''.join(sequence2[index:index+CHUNK_SIZE]))
+	while MAX_CHARS_PER_LINE < remaining_list_size:
+		print (''.join(sequence1[index:index+MAX_CHARS_PER_LINE]))
+		print (''.join(match_characters[index:index+MAX_CHARS_PER_LINE]))
+		print (''.join(sequence2[index:index+MAX_CHARS_PER_LINE]))
 		print("")
-		index += CHUNK_SIZE
-		remaining_list_size -= CHUNK_SIZE
+		index += MAX_CHARS_PER_LINE
+		remaining_list_size -= MAX_CHARS_PER_LINE
 	print (''.join(sequence1[index:]))
 	print (''.join(match_characters[index:]))
 	print (''.join(sequence2[index:]))
-
-	
-
-
-
+	print ("")
 
 
 if __name__ == "__main__":
